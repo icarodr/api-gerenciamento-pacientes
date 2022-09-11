4e0e56ece0d93287f9de62d299f97d7608655071
icarodr <it46009@gmail.com>
2022-09-11 20:22:12 -0300

    added model Pacientes

---
+++ pacientes/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Pacientes(models.Model):
+    id_paciente = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100, blank=True, null=True)
+    data_nasc = models.DateTimeField(blank=True, null=True)
+    endereco = models.CharField(max_length=80, blank=True, null=True)
+    num_endereco = models.IntegerField(blank=True, null=True)
+    bairro_endereco = models.CharField(max_length=60, blank=True, null=True)
+    cep = models.CharField(max_length=100, blank=True, null=True)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+    rg = models.CharField(max_length=100, blank=True, null=True)
+
+    #Todos os tipos de Field estão na documentação do Django
+
+    class Meta:
+        managed = True
+        db_table = 'pacientes'
+
+    
