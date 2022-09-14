0dea388e34a4e320a0eb206e49c670939f570918
icarodr <it46009@gmail.com>
2022-09-14 11:08:42 -0300

    add static files on urls

--- clinica/urls.py
+++ clinica/urls.py
@@ -4,13 +4,17 @@ from rest_framework import routers
 from pacientes.api.viewsets import PacientesViewSet
 from agendamentos.api.viewsets import AgendamentoViewSet
 from historicos.api.viewsets import HistoricosViewSet
+from imagens.api.viewsets import ImagensHistoricoViewSet
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = routers.DefaultRouter()
 router.register(r'pacientes', PacientesViewSet)
 router.register(r'agendamentos', AgendamentoViewSet)
 router.register(r'historicos', HistoricosViewSet)
+router.register(r'imagens_historicos', ImagensHistoricoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
