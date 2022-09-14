fc14466417b0a27e8f54eb168d4863403635eb7f
icarodr <it46009@gmail.com>
2022-09-14 11:08:11 -0300

    add actions in agendamento and viewset

--- agendamentos/api/serializers.py
+++ agendamentos/api/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
 from agendamentos.models import Agendamentos
+from historicos.api.serializers import HistoricosSerializer
 
 
 class AgendamentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Agendamentos
         fields = '__all__'
+
+class AgendamentosDetalhesSerializer(serializers.ModelSerializer):
+    historicos = HistoricosSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Agendamentos
+        fields = [
+            'id_agendamento',
+            'data_hora',
+            'data_criacao',
+            'cancelado',
+            'obs',
+            'tipo',
+            'historicos'
+        ]
--- agendamentos/api/viewsets.py
+++ agendamentos/api/viewsets.py
@@ -1,7 +1,18 @@
 from rest_framework import viewsets
 from agendamentos.models import Agendamentos
 from agendamentos.api.serializers import AgendamentoSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from agendamentos.api.serializers import AgendamentosDetalhesSerializer
 
 class AgendamentoViewSet(viewsets.ModelViewSet):
     queryset = Agendamentos.objects.all().order_by('data_hora')
     serializer_class = AgendamentoSerializer
+
+    @action(detail=True, methods=['get'])
+    def detalhes(self, request, pk=None, *args, **kwargs):
+        queryset = Agendamentos.objects.filter(pk=pk)
+        self.serializer_class = AgendamentosDetalhesSerializer
+        serializer = self.get_serializer(queryset, many=True)
+        
+        return Response(serializer.data)
